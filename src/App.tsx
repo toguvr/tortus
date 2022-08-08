413828372254654e9a1196604b8136e2948243f5
Augusto Telles <augustotf93@gmail.com>
2022-08-07 21:08:23 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [possibles, setPossibles] = useState(
+    Array.from({ length: 49 }, (_, index) => index + 1)
+  );
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +13,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="tabuleiro">
+          {possibles.map((possible) => (
+            <div className="casa">{possible}</div>
+          ))}
+        </div>
       </header>
     </div>
   );
